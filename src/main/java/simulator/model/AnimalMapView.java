5f214fb18865a41156e66657440959d0ac2e4d3a
aveny-maker <aveny@ucm.es>
2026-02-05 11:27:05 +0100

    c

--- src/main/java/simulator/model/AnimalMapView.java
+++ src/main/java/simulator/model/AnimalMapView.java
@@ -1,5 +1,8 @@
 package simulator.model;
 
-public interface AnimalMapView extends MapInfo, FoodSupplier {
+import java.util.List;
+import java.util.function.Predicate;
 
+public interface AnimalMapView extends MapInfo, FoodSupplier {
+    public List<Animal> getAnimalsInRange(Animal e, Predicate<Animal> filter);
 }
